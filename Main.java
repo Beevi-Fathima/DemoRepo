a65a8b2b995a5c06278a487371e813a3344c596b
Beevi-Fathima <122143182+Beevi-Fathima@users.noreply.github.com>
2023-06-15 14:51:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First line from the app");
+		System.out.println("Second line from the app");
+		System.out.println("Third line from the app");
+
+	}
+
+}
